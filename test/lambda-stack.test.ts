01a43cf45f18d990f1516fcc2258b2a4bf03eeb0
wildDoubt <coma01155@gmail.com>
2023-02-01 10:04:04 +0900

    test: create lambda stack test code

--- test/lambda-stack.test.ts
+++ test/lambda-stack.test.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { LambdaStack } from '../lib/lambda-stack';
 
+const LAMBDA_FUNCTION = 'AWS::Lambda::Function';
+const EVENT_RULE = 'AWS::Events::Rule';
 test('Lambda created', () => {
    // given
    const app = new cdk.App();
@@ -11,5 +13,31 @@ test('Lambda created', () => {
    const template = Template.fromStack(stack);
 
    // then
-   template.resourceCountIs('AWS::Lambda::Function', 3);
+   template.resourceCountIs(LAMBDA_FUNCTION, 3);
+   // rule이 1개 생성
+   // 람다 함수가 rule의 target이 되었는지
+});
+
+test('Event rule created', () => {
+   // given
+   const app = new cdk.App();
+   const stack = new LambdaStack(app, 'LambdaStack');
+
+   // when
+   const template = Template.fromStack(stack);
+
+   // then
+   template.resourceCountIs(EVENT_RULE, 1);
+});
+
+test('Lambda functions targeted to event rule', () => {
+   // given
+   const app = new cdk.App();
+   const stack = new LambdaStack(app, 'LambdaStack');
+
+   // when
+   const template = Template.fromStack(stack);
+
+   // then
+   template.resourcePropertiesCountIs(LAMBDA_FUNCTION, {}, 3);
 });
